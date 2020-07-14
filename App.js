775a96f588a951f7314cbd9f26ff47e4764221c4
Itech <ibrahimmahamatmoussa@gmail.com>
2020-07-14 14:17:57 +0100

    First commit

---
+++ App.js
@@ -0,0 +1,32 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.input} placeholder="Course goal" />
+        <Button title="ADD" />
+      </View>
+      <View />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  input: {
+    width: "80%",
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
+  },
+});
